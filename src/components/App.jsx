86d59a9907ce0b6a2c94239582bfbd0b408b5f03
ahsan.habib <ahsan.habib@brainstation-23.com>
2019-01-23 18:20:40 +0600

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,35 @@ import React from "react";
 import youtube from "../apis/youtube";
 
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: { q: term }
     });
-    console.log(response.data);
+    this.setState({ videos: response.data.items });
   };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
